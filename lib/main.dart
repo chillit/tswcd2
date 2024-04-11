1f1e10eefbf136f5df2635c856cdf1fe17564232
Akanov-Kanta <129672907+Akanov-Kanta@users.noreply.github.com>
2024-04-11 13:23:27 +0500

    futura as a defaut font

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        fontFamily: "Futura",
         // This is the theme of your application.
         //
         // TRY THIS: Try running your application with "flutter run". You'll see
